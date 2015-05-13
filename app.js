dd644ad078c0d4b52037c502123d3a48de45d1b9
Moufid Larbi <moufid4@gmail.com>
2015-05-14 00:29:31 +0100

    adding server for static files

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var weatherApp = angular.module('weatherApp', ['ngRoute','ngResource']);
 // routes
 weatherApp.config(function ($routeProvider) {
 	$routeProvider
-		.when('/' {
+		.when('/', {
 			templateUrl: 'pages/home.html',
 			controller: 'homeController'
 		})
 
-		.when('/forecast' {
+		.when('/forecast', {
 			templateUrl: 'pages/forecast.html',
 			controller: 'forecastController'
 		})
