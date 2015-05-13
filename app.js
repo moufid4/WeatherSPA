5a938c4417f805b4c42eb7f1f5b71dc29f9c36b8
Moufid Larbi <moufid4@gmail.com>
2015-05-14 00:16:34 +0100

    adding a home and forecast controllers and views

---
+++ app.js
@@ -0,0 +1,13 @@
+// module
+var weatherApp = angular.module('weatherApp', ['ngRoute','ngResource']);
+
+// controllers
+
+weatherApp.controller('homeController' ['$scope', function($scope) {
+
+}]);
+
+weatherApp.controller('forecastController' ['$scope', function($scope) {
+
+}]);
+
