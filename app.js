7d5ed620aaacf3a27010bcb8b55dcbebb5489445
Moufid Larbi <moufid4@gmail.com>
2015-05-14 00:59:45 +0100

    building a custom service to share data between home and forecast views

--- app.js
+++ app.js
@@ -13,13 +13,29 @@ weatherApp.config(function ($routeProvider) {
 			templateUrl: 'pages/forecast.html',
 			controller: 'forecastController'
 		})
-})
-// controllers
-weatherApp.controller('homeController' ['$scope', function($scope) {
+});
 
+// services
+weatherApp.service('cityService', function() {
+
+	this.city = "Toronto, ON"
+
+});
+
+// controllers
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+
+	$scope.city = cityService.city;
+
+	$scope.$watch('city', function() {
+
+		cityService.city = $scope.city;
+	});
 }]);
 
-weatherApp.controller('forecastController' ['$scope', function($scope) {
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+
+	$scope.city = cityService.city;
 
 }]);
 
